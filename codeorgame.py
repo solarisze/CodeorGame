bd0384d794549696030fa31d24d3162bb19088a7
solarisze <95149150+solarisze@users.noreply.github.com>
2021-12-02 13:23:49 -0500

    add the actual code start

---
+++ codeorgame.py
@@ -0,0 +1,2 @@
+print("You are sitting in the library in fifth period. You have a study hall, so there's nothing you're required to do at the moment. You have a couple options until school ends, in an hour. You can wait for your father to purchase video games and send you the Stealth codes for them, or you can code something in Viper. What will you do?")
+choice1 = input("('Code' to play around with Viper, 'Wait' to wait for the video games.)")
